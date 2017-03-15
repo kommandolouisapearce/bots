c138fbe7ca5350ce375f23331f990e56f75f44c1
fredrikbryntesson <fredrik.bryntesson@vidhance.com>
2017-03-15 22:46:22 +0100

    Started on call for reading whole channel history

---
+++ readchannel.js
@@ -0,0 +1,6 @@
+
+
+//https://api.slack.com/methods/channels.history
+
+//If you need to do this dynamically via API you can use the channels.list method to list all of the channels in your team 
+//and channels.history method to retrieve the history of each channel. Note that this will not include DMs or private groups.
